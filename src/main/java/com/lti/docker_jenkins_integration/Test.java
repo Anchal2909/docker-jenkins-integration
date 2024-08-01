8b3eb88ddcdb5758ae9d0eae865086e1e54100f5
Anchal2909 <anchalsing1@gmail.com>
2024-08-01 16:39:54 +0530

    second commit

---
+++ src/main/java/com/lti/docker_jenkins_integration/Test.java
@@ -0,0 +1,7 @@
+package com.lti.docker_jenkins_integration;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello from ....docker-jenkins-integration demo.");
+    }
+}
